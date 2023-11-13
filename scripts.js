5ec6c3a5ac777781b5f678bf34939c3134c86cee
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com.>
2023-11-13 15:41:51 +0200

    finished with grid sizing and grid sizing specification feature

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
-window.addEventListener("contextmenu", (e) => e.preventDefault());
 window.onload = function () {
   const container = document.querySelector(".board");
+  const size = document.querySelector(".range");
+  const rangeSpecific = document.querySelector("#range__specific");
+  size.addEventListener("change", function (e) {
+    // Remove all existing squares
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    // Create a new grid with the selected size
+    createGrid(e.target.value);
+  });
+
+  const color = document.querySelector(".color");
+  paintGrid(color.value);
+  color.addEventListener("change", function (e) {
+    paintGrid(e.target.value);
+  });
 
   function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
@@ -10,7 +25,12 @@ window.onload = function () {
     }
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    rangeSpecific.textContent = `${size} X ${size}`;
   }
-
-  createGrid(60);
+  createGrid(16);
 };
+
+function paintGrid(color) {}
+
+window.addEventListener("contextmenu", (e) => e.preventDefault());
+// disables contextmenu
